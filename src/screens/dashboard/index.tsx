bc4760c9622c8a31822c90dd99af45bee8429a06
suhil mohammad <smohammad@armstrongfluidtechnology.com>
2024-10-24 10:58:17 -0400

    added skeleton loaders

--- src/screens/dashboard/index.tsx
+++ src/screens/dashboard/index.tsx
@@ -1,14 +1,15 @@
-import { Stack } from "@mui/material";
+import { Skeleton, Stack } from "@mui/material";
 import Intro from "./Intro";
 import MenuSection from "./MenuSection";
 import useMenuData from "@hooks/useMenuData";
 
 export default function DashboardScreen() {
-  const { menuSections } = useMenuData();
+  const { menuSections, loading } = useMenuData();
 
   return (
-    <Stack sx={{ mx: 2, my: 1 }}>
+    <Stack sx={{ mx: 2, my: 2 }}>
       <Intro />
+      {loading && <MenuSkeletons />}
       {menuSections?.map((section) => (
         <MenuSection
           key={section.section}
@@ -19,3 +20,19 @@ export default function DashboardScreen() {
     </Stack>
   );
 }
+
+const MenuSkeletons = () => (
+  <Stack>
+    <Skeleton variant="text" width={200} height={50} sx={{ mb: 2, mt: 5 }} />
+    <Skeleton
+      variant="rounded"
+      width={"100%"}
+      sx={{ mb: 2, height: { xs: 200, sm: 150 } }}
+    />
+    <Skeleton
+      variant="rounded"
+      width={"100%"}
+      sx={{ height: { xs: 200, sm: 150 } }}
+    />
+  </Stack>
+);
